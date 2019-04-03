666c8f23c01e9615aca448b024f8117719bb50ba
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-03 23:27:08 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,53 @@
 declare const pTap: {
 	/**
-	 * Tap into a promise chain without affecting its value or state. Use this in a `.then()` method.
-	 *
-	 * @param tapHandler - Any return value is ignored. Exceptions thrown in `tapHandler` are relayed back to the original promise chain. If `tapHandler` returns a `Promise`, it will be awaited before passing through the original value.
-	 * @returns A [thunk](https://en.wikipedia.org/wiki/Thunk) that returns a `Promise`.
-	 */
+	Tap into a promise chain without affecting its value or state. Use this in a `.then()` method.
+
+	@param tapHandler - Any return value is ignored. Exceptions thrown in `tapHandler` are relayed back to the original promise chain. If `tapHandler` returns a `Promise`, it will be awaited before passing through the original value.
+	@returns A [thunk](https://en.wikipedia.org/wiki/Thunk) that returns a `Promise`.
+
+	@example
+	```
+	import pTap = require('p-tap');
+
+	Promise.resolve('unicorn')
+		.then(pTap(console.log)) // Logs `unicorn`
+		.then(value => {
+			// `value` is still `unicorn`
+		});
+
+	getUser()
+		.then(pTap(user => recordStatsAsync(user))) // Stats are saved about `user` async before the chain continues
+		.then(user => {
+			// `user` is the user from getUser(), not recordStatsAsync()
+		});
+	```
+	*/
 	<ValueType = unknown>(tapHandler: (value: ValueType) => unknown): (
 		value: ValueType
 	) => Promise<ValueType>;
 
 	/**
-	 * Tap into a promise chain without affecting its value or state. Use this in a `.catch()` method.
-	 *
-	 * @param tapHandler - Any return value is ignored. Exceptions thrown in `tapHandler` are relayed back to the original promise chain. If `tapHandler` returns a `Promise`, it will be awaited before passing through the original value.
-	 * @returns A [thunk](https://en.wikipedia.org/wiki/Thunk) that returns a `Promise`.
-	 */
+	Tap into a promise chain without affecting its value or state. Use this in a `.catch()` method.
+
+	@param tapHandler - Any return value is ignored. Exceptions thrown in `tapHandler` are relayed back to the original promise chain. If `tapHandler` returns a `Promise`, it will be awaited before passing through the original value.
+	@returns A [thunk](https://en.wikipedia.org/wiki/Thunk) that returns a `Promise`.
+
+	@example
+	```
+	import pTap = require('p-tap');
+
+	Promise.resolve(() => doSomething())
+		.catch(pTap.catch(console.error)) // prints any errors
+		.then(handleSuccess)
+		.catch(handleError);
+	```
+	*/
 	catch<ErrorType = unknown>(
 		tapHandler: (error: ErrorType) => unknown
 	): (error: ErrorType) => Promise<never>;
-}
 
-export default pTap;
+	// TODO: Remove this for the next major release
+	default: typeof pTap;
+};
+
+export = pTap;
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,5 @@
-import {expectType} from 'tsd-check';
-import pTap from '.';
+import {expectType} from 'tsd';
+import pTap = require('.');
 
 Promise.resolve('unicorn')
 	.then(
